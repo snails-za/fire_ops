d38f186418a5e6fc21bf96a235b8d80368df90e3
snails-za <wangjuchn@outlook.com>
2024-11-16 02:14:41 +0800

    入口文件支持调试

--- app/asgi.py
+++ app/asgi.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from apps import init_db, init_routes, init_cors, init_static
@@ -21,4 +22,8 @@ def create_app():
 
 
 app = create_app()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 

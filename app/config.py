dfe19316a6ce7c30e95f58e62ea0e104469d96a7
snails-za <wangjuchn@outlook.com>
2024-11-16 17:54:25 +0800

    数据库配置修改

--- app/config.py
+++ app/config.py
@@ -11,12 +11,11 @@ DEBUG = config("DEBUG", cast=bool, default=False)
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 STATIC_PATH = os.path.join(BASE_PATH, "static")
 # 数据库配置
-DB_DRIVER = config("DB_DRIVER", default="postgresql")
-DB_HOST = config("DB_HOST", default="localhost")
-DB_PORT = config("DB_PORT", cast=int, default=15432)
-DB_USER = config("DB_USER", default="postgres")
-DB_PASSWORD = Secret(Base64Util.decode(config("DB_PASSWORD", cast=str, default="OTcxMDEx")))
-DB_DATABASE = config("DB_DATABASE", default="fastapi_demo")
+DB_HOST = config("POSTGRES_HOST", default="localhost")
+DB_PORT = config("POSTGRES_PORT", cast=int, default=15432)
+DB_USER = config("POSTGRES_USER", default="postgres")
+DB_PASSWORD = Secret(Base64Util.decode(config("POSTGRES_PASSWORD", cast=str, default="OTcxMDEx")))
+DB_DATABASE = config("POSTGRES_DB", default="fastapi_demo")
 
 
 

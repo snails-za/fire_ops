2ec043db8990cc541688e3af143e19d4ad59771a
wangju <wangjuchn@outlook.com>
2025-04-11 22:17:12 +0800

    数据库配置修改

--- app/config.py
+++ app/config.py
@@ -3,8 +3,6 @@ import os
 from starlette.config import Config
 from pydantic import Secret
 
-from apps.utils.common import Base64Util
-
 config = Config()
 
 DEBUG = config("DEBUG", cast=bool, default=False)
@@ -14,7 +12,7 @@ STATIC_PATH = os.path.join(BASE_PATH, "static")
 DB_HOST = config("POSTGRES_HOST", default="localhost")
 DB_PORT = config("POSTGRES_PORT", cast=int, default=15432)
 DB_USER = config("POSTGRES_USER", default="postgres")
-DB_PASSWORD = Secret(Base64Util.decode(config("POSTGRES_PASSWORD", cast=str, default="OTcxMDEx")))
+DB_PASSWORD = Secret(config("POSTGRES_PASSWORD", cast=str, default="OTcxMDEx"))
 DB_DATABASE = config("POSTGRES_DB", default="fastapi_demo")
 DATABASE_URL = f"postgres://{DB_USER}:{DB_PASSWORD.get_secret_value()}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
 TORTOISE_ORM = {

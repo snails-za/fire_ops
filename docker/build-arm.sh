1cd75641d938b5a5943707bb3dda6ee69a7e4fa8
wangju <wangjuchn@outlook.com>
2025-04-11 22:19:33 +0800

    arm脚本提交

--- docker/build-arm.sh
+++ docker/build-arm.sh
@@ -3,7 +3,7 @@ cd ..
 
 TIME_NOW=`date +%Y%m%d%H%M`
 # 生成镜像
-#docker build --no-cache --progress=plain -f docker/Dockerfile -t snail97/fastapi_demo:${TIME_NOW} .
+#docker build --no-cache --progress=plain -f docker/Dockerfile-arm -t snail97/fastapi_demo_arm:${TIME_NOW} .
 docker build --progress=plain -f docker/Dockerfile-arm -t snail97/fastapi_demo_arm:${TIME_NOW} .
 # 打标签
 docker tag snail97/fastapi_demo_arm:${TIME_NOW} snail97/fastapi_demo_arm:latest

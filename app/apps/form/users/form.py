6639c4b2906ab0ecf69458600bf1a414ba9f8715
wangju <wangjuchn@outlook.com>
2025-04-13 15:36:26 +0800

    增加form说明

--- app/apps/form/users/form.py
+++ app/apps/form/users/form.py
@@ -1,13 +1,16 @@
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, Field
 
 
 class UserBase(BaseModel):
-    username: str
-    email: str
+    username: str = Field(..., min_length=3, max_length=20, title="用户名", description="用户名长度在3到20之间")
+    email: Optional[str] = Field(None, pattern=r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", title="邮箱",
+                       description="邮箱格式不正确")
 
 
 class UserCreate(UserBase):
-    password: str
+    password: str = Field(..., min_length=8, max_length=20, title="密码", description="密码长度在8到20之间")
 
 
 class UserInDB(UserBase):
--- app/apps/models/user.py
+++ app/apps/models/user.py
@@ -5,5 +5,5 @@ from tortoise.models import Model
 class User(Model):
     id = fields.IntField(pk=True)
     username = fields.CharField(max_length=20, unique=True)
-    email = fields.CharField(max_length=50, unique=True)
+    email = fields.CharField(null=True, max_length=50, unique=True)
     hashed_password = fields.CharField(max_length=128)

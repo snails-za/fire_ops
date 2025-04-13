e0474e3347e3d6277cc803f93d29a6f6b584f4e8
wangju <wangjuchn@outlook.com>
2025-04-13 15:18:52 +0800

    替换dict为model_dump

--- app/apps/utils/__init__.py
+++ app/apps/utils/__init__.py
@@ -6,9 +6,9 @@ from fastapi.responses import JSONResponse
 def serialize_data(data: Any) -> Any:
     # 自动处理 Pydantic 模型或模型列表
     if isinstance(data, list):
-        return [d.dict() if hasattr(d, "dict") else d for d in data]
-    if hasattr(data, "dict"):
-        return data.dict()
+        return [d.model_dump() if hasattr(d, "model_dump") else d for d in data]
+    if hasattr(data, "model_dump"):
+        return data.model_dump()
     return data
 
 

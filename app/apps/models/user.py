de69f3ed46734fcc808163bede5a76571878e5ae
wangju <wangjuchn@outlook.com>
2025-06-21 20:44:39 +0800

    is_accept改为可以为null

--- app/apps/models/user.py
+++ app/apps/models/user.py
@@ -19,7 +19,7 @@ class Contact(BaseModel):
     contact = fields.ForeignKeyField("models.User", related_name="contact", on_delete=fields.CASCADE,
                                      description="联系人")
     is_star = fields.BooleanField(default=False, description="是否星标")
-    is_accept = fields.BooleanField(default=False, description="是否接受")
+    is_accept = fields.BooleanField(default=None, null=True, description="是否接受")
     bak = fields.TextField(null=True, description="备注")
 
 

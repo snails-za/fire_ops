b7416fdde0cb719545994af00a98f2ee26440760
wangju <wangjuchn@outlook.com>
2025-08-22 21:06:49 +0800

    打标签

--- docker/build-arm.sh
+++ docker/build-arm.sh
@@ -6,5 +6,5 @@ TIME_NOW=`date +%Y%m%d%H%M`
 #docker build --no-cache --progress=plain -f docker/Dockerfile-arm -t snail97/fire_ops_arm:${TIME_NOW} .
 docker build -f docker/Dockerfile-arm -t snail97/fire_ops_arm:${TIME_NOW} .
 # 打标签
-#docker tag snail97/fire_ops_arm:${TIME_NOW} snail97/fire_ops_arm:latest
+docker tag snail97/fire_ops_arm:${TIME_NOW} snail97/fire_ops_arm:latest
 

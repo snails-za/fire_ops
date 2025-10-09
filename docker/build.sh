7ef4899c89fa7bf1c542a5db97e187d90109074e
wangju <wangjuchn@outlook.com>
2025-10-09 22:18:40 +0800

    build.sh修改

--- docker/build.sh
+++ docker/build.sh
@@ -4,8 +4,8 @@ cd ..
 TIME_NOW=`date +%Y%m%d%H%M`
 # 生成镜像
 #docker build --no-cache --progress=plain -f docker/Dockerfile -t snail97/fire-ops:${TIME_NOW} .
-docker build -f docker/Dockerfile -t snail97/fire-ops:latest .
-#docker build -f docker/Dockerfile -t snail97/fire-ops:${TIME_NOW} .
+docker build -f docker/Dockerfile -t snail97/fire_ops:latest .
+#docker build -f docker/Dockerfile -t snail97/fire_ops:${TIME_NOW} .
 ## 打标签
-#docker tag snail97/fire-ops:${TIME_NOW} snail97/fire-ops:latest
+#docker tag snail97/fire-ops:${TIME_NOW} snail97/fire_ops:latest
 
